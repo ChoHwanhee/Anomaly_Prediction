31c2c0c8486b9181968781382b4c83ff75cfd23c
Hwanhee Cho <hwanheecho@Hwanheeui-MacBookPro.local>
2023-11-25 08:22:11 +0900

    first commit

--- train.py
+++ train.py
@@ -27,8 +27,8 @@ parser.add_argument('--dataset', default='avenue', type=str, help='The name of t
 parser.add_argument('--iters', default=40000, type=int, help='The total iteration number.')
 parser.add_argument('--resume', default=None, type=str,
                     help='The pre-trained model to resume training with, pass \'latest\' or the model name.')
-parser.add_argument('--save_interval', default=1000, type=int, help='Save the model every [save_interval] iterations.')
-parser.add_argument('--val_interval', default=1000, type=int,
+parser.add_argument('--save_interval', default=1, type=int, help='Save the model every [save_interval] iterations.')
+parser.add_argument('--val_interval', default=1, type=int,
                     help='Evaluate the model every [val_interval] iterations, pass -1 to disable.')
 parser.add_argument('--show_flow', default=False, action='store_true',
                     help='If True, the first batch of ground truth optic flow could be visualized and saved.')
